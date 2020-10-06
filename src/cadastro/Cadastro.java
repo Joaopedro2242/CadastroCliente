361e12ce7b1a65b4ac16b61e4f4231726e6301ea
unknown <joao22pedro22@hotmail.com>
2020-10-05 21:49:08 -0300

    nisbqieuwb

---
+++ src/cadastro/Cadastro.java
@@ -0,0 +1,12 @@
+
+package cadastro;
+
+public class Cadastro {
+
+    public static void main(String[] args) {
+    Pessoa cliente = new Pessoa("João Pedro", "12345678", "Masculino", "Rua ana carmela jurado ferro, 425", "Indaiatuba", "Sp");
+    
+    
+    }
+    
+}
---
+++ src/cadastro/Pessoa.java
@@ -0,0 +1,78 @@
+package cadastro;
+
+public class Pessoa {
+    private int id;
+    private String nome;
+    private String cpf;
+    private String sexo;
+    private String endereço;
+    private String cidade;
+    private String estado;
+
+    public Pessoa(String nome, String cpf, String sexo, String endereço, String cidade, String estado) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.sexo = sexo;
+        this.endereço = endereço;
+        this.cidade = cidade;
+        this.estado = estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
+}
